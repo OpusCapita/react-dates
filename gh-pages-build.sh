46884ed93c3d30b8b133bb20e3a54867ace6e00a
Kirill Volkovich <volkovich@scand.com>
2017-04-20 16:20:18 +0300

    Update gh-pages-build.sh

--- gh-pages-build.sh
+++ gh-pages-build.sh
@@ -7,12 +7,15 @@ cp lib/* .gh-pages-tmp &&
 cp www/index.html .gh-pages-tmp &&
 
 git checkout gh-pages &&
+echo "LS 1!" && ls -la &&
 git ls-files | grep -v -e "\(^\.gitignore$\|^\.gitattributes$\|^\.gh-pages-tmp$\)" | xargs rm -rf &&
-echo "LS!" &&
-ls -la &&
+echo "LS 2!" && ls -la &&
 mv .gh-pages-tmp/* . &&
+echo "LS 3!" && ls -la &&
 rm -rf .gh-pages-tmp &&
+echo "LS 4!" && ls -la &&
 git add . &&
+echo "LS 5!" && ls -la &&
 git commit -m "Update gh-pages" &&
 git push --force origin gh-pages &&
 git checkout master
