7896a79e003741fb7a3f5b959dbc7a129faa370f
Kirill Volkovich <volkovich@scand.com>
2017-04-20 15:07:53 +0300

    Update gh-pages-build.sh

--- gh-pages-build.sh
+++ gh-pages-build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-rm -rf .gh-pages-tmp
-mkdir .gh-pages-tmp
-node node_modules/webpack/bin/webpack.js --config ./webpack.docs.config.js
-cp lib/* .gh-pages-tmp
-cp www/index.html .gh-pages-tmp
+rm -rf .gh-pages-tmp  &&
+mkdir .gh-pages-tmp &&
+node node_modules/webpack/bin/webpack.js --config ./webpack.docs.config.js &&
+cp lib/* .gh-pages-tmp &&
+cp www/index.html .gh-pages-tmp &&
 
-git checkout gh-pages
-git checkout master -- .gh-pages-tmp
-mv .gh-pages-tmp/* .
-rm -rf .gh-pages-tmp
-git push --force origin/gh-pages
+git checkout gh-pages &&
+git checkout master -- .gh-pages-tmp &&
+mv .gh-pages-tmp/* . &&
+rm -rf .gh-pages-tmp &&
+git push --force origin/gh-pages &&
 
-git checkout master
+git checkout master &&
 rm -rf .gh-pages-tmp
