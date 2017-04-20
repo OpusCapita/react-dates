837bcc3d8937504081402962b550a9855ec02ca9
Kirill Volkovich <volkovich@scand.com>
2017-04-20 15:17:13 +0300

    Update gh-pages-build.sh

--- gh-pages-build.sh
+++ gh-pages-build.sh
@@ -7,10 +7,7 @@ cp lib/* .gh-pages-tmp &&
 cp www/index.html .gh-pages-tmp &&
 
 git checkout gh-pages &&
-git checkout master -- .gh-pages-tmp &&
 mv .gh-pages-tmp/* . &&
 rm -rf .gh-pages-tmp &&
 git push --force origin/gh-pages &&
-
-git checkout master &&
-rm -rf .gh-pages-tmp
+git checkout master
