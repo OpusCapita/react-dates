ec856270d7ca3b1e24d67784429b6f8e0e4a18d8
Kirill Volkovich <volkovich@scand.com>
2017-04-21 17:26:03 +0300

    Fix I18nLinker default datePattern

--- src/client/components/I18nLinker/I18nLinker.react.js
+++ src/client/components/I18nLinker/I18nLinker.react.js
@@ -4,7 +4,7 @@ export default
 class I18nLinker extends Component {
   render() {
     let locale = this.context.i18n.locale || 'en-GB';
-    let dateFormat = this.context.i18n._formatInfo.datePattern || 'en-GB';
+    let dateFormat = this.context.i18n._formatInfo.datePattern || 'dd/MM/yyyy';
     let child = React.Children.only(this.props.children);
     return React.cloneElement(child, { locale, dateFormat });
   }
