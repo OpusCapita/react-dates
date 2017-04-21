ae5bbe7c60cec7653958bcf769c27360cc603ec3
Kirill Volkovich <volkovich@scand.com>
2017-04-21 17:29:57 +0300

    Update gh-pages-updates.sh

--- gh-pages-update.sh
+++ gh-pages-update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -rf .gh-pages-tmp  &&
+rm -rf .gh-pages-tmp lib  &&
 mkdir .gh-pages-tmp &&
 node node_modules/webpack/bin/webpack.js --config ./webpack.docs.config.js &&
 cp lib/* .gh-pages-tmp &&
