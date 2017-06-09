4e70d3a57e4d1b01eb64ca741f995c8c391dbb42
Alexey Sergeev <sab@scand.com>
2017-06-09 09:05:19 +0300

    Building correct index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,7 @@
-// Here should be CommonJS component exports.
+// Here should be es2016 or CommonJS exports.
 
 module.exports = {
-
+  DateInput: require('./components/DateInput').default,
+  DateRangeInput: require('./components/DateRangeInput').default,
+  DatePicker: require('./components/DatePicker').default
 };
