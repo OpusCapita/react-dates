cf6bd7cb8fea7cffe6743e677a6c96c81db799be
Anna Motuz <amotuz-sc@users.noreply.github.com>
2017-09-29 19:06:26 +0300

    Added FI/NO/SV translations.

--- src/client/components/translations.js
+++ src/client/components/translations.js
@@ -37,6 +37,45 @@ const translations = {
     last30Days: 'Последние 30 дней',
     thisMonth: 'Этот месяц',
     nextMonth: 'Следующий месяц'
+  },
+  fi: {
+    selectDateRange: 'Valitse päivämääräväli',
+    today: 'Tänään',
+    yesterday: 'Eilen',
+    tomorrow: 'Huomenna',
+    previousWeek: 'Edellinen viikko',
+    nextWeek: 'Seuraava viikko',
+    thisWeek: 'Tämä viikko',
+    previousMonth: 'Edellinen kuukausi',
+    last30Days: 'Edelliset 30 päivää',
+    thisMonth: 'Tämä kuukausi',
+    nextMonth: 'Seuraava kuukausi'
+  },
+  no: {
+    selectDateRange: 'Velg datoområde',
+    today: 'I dag',
+    yesterday: 'I går',
+    tomorrow: 'I morgen',
+    previousWeek: 'Forrige uke',
+    nextWeek: 'Neste uke',
+    thisWeek: 'Denne uken',
+    previousMonth: 'Foregående måned',
+    last30Days: 'Siste 30 dager',
+    thisMonth: 'Denne måneden',
+    nextMonth: 'Neste måned'
+  },
+   sv: {
+    selectDateRange: 'Välj datumintervall',
+    today: 'Idag',
+    yesterday: 'Igår',
+    tomorrow: 'Imorgon',
+    previousWeek: 'Föregående vecka',
+    nextWeek: 'Nästa vecka',
+    thisWeek: 'Denna vecka',
+    previousMonth: 'Föregående månad',
+    last30Days: 'SSenaste 30 dagarna',
+    thisMonth: 'Denna månad',
+    nextMonth: 'Nästa månad'
   }
 };
 
