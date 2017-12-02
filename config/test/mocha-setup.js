d143b1868aadcd0902cc0130d905332e03e8a466
Kirill Volkovich <kirill.volkovich@opuscapita.com>
2017-12-02 14:28:03 +0300

    Fix run test

--- config/test/mocha-setup.js
+++ config/test/mocha-setup.js
@@ -1,3 +1,6 @@
+import Enzyme  from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 const { JSDOM } = require('jsdom');
 const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
 const { window } = jsdom;
@@ -9,14 +12,28 @@ function copyProps(src, target) {
   Object.defineProperties(target, props);
 }
 
+/*
+   "Warning: React depends on requestAnimationFrame.
+   Make sure that you load a polyfill in older browsers. http://fb.me/react-polyfills"
+*/
+global.requestAnimationFrame = (callback) => {
+    setTimeout(callback, 16);
+};
+
 global.window = window;
 global.document = window.document;
 global.navigator = {
   userAgent: 'node.js'
 };
+
+global.self = global.window;
+
+
 copyProps(window, global);
 
 var chai = require('chai');
 
 chai.use(require('sinon-chai'));
 chai.use(require('chai-enzyme'));
+
+Enzyme.configure({ adapter: new Adapter() });
